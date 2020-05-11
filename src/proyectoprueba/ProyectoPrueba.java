99f7f1241c90f9c7ef63985086937cd74a2447b6
Marcela Guerrero <marcelaguerrero1396@gmail.com>
2020-05-10 21:48:48 -0500

    Primer commit del proyecto ejemplo

---
+++ src/proyectoprueba/ProyectoPrueba.java
@@ -0,0 +1,19 @@
+package proyectoprueba;
+
+import javax.swing.JFrame;
+
+
+public class ProyectoPrueba {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int mul, number = 10;
+        for (int i = 1; i<=10; i ++){
+            mul = number * i;
+            System.out.println(mul);
+        }
+    }
+    
+}
